27c9097aedbbf5922d218fcca12b1d598e22e67b
Yaroslavplaksii <yaroslav.plaksii@gmail.com>
2018-02-14 09:41:11 +0200

    lasr

--- partial1/app.js
+++ partial1/app.js
@@ -1,9 +1,9 @@
 var app = angular.module('sampleApp',[]);
 app.controller('SampleAppCtr',function($scope){
     $scope.items = [{"name":"n1","value":"v1"},
-        {"name":"n2","value":"v2"},
-        {"name":"n3","value":"v3"},
-        {"name":"n4","value":"v4"}];
+                    {"name":"n2","value":"v2"},
+                    {"name":"n3","value":"v3"},
+                    {"name":"n4","value":"v4"}];
 
     $scope.url = "table.html";
 });
